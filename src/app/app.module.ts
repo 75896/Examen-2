2add55163dd967a693aacef6c996994dd1e520da
jose <josejulianquinteros@gmail.com>
2024-06-05 18:26:59 -0400

    examen Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SliderComponent } from './slider/slider.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SliderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
